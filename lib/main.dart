7e066e6ff98f7d77285eeaead85444e947e83160
dherediat <dherediat97@gmail.com>
2025-04-17 19:44:46 +0200

    feature: add updated model, fix some things of the main view

--- lib/main.dart
+++ lib/main.dart
@@ -3,42 +3,49 @@ import 'package:model_viewer_plus/model_viewer_plus.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  runApp(const HorusVisionEngine());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HorusVisionEngine extends StatefulWidget {
+  const HorusVisionEngine({super.key});
 
+  @override
+  State<HorusVisionEngine> createState() => _HorusVisionEngineState();
+}
+
+class _HorusVisionEngineState extends State<HorusVisionEngine> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Horus Vision Engine',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(),
+      home: ImmersiveView(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class ImmersiveView extends StatefulWidget {
+  const ImmersiveView({super.key, this.modelSrc = 'assets/immersive_film.glb'});
 
+  final String modelSrc;
+
+  @override
+  State<ImmersiveView> createState() => _ImmersiveViewState();
+}
+
+class _ImmersiveViewState extends State<ImmersiveView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Horus Vision Engine'),
-      ),
-      body: const ModelViewer(
-        backgroundColor: Colors.black87,
-        src: 'assets/immersive_film.glb',
+      body: ModelViewer(
+        src: widget.modelSrc,
         alt: 'Immersive film',
         ar: true,
         arScale: ArScale.fixed,
         cameraControls: true,
-        autoRotate: true,
+        autoRotate: false,
         disableZoom: true,
         disablePan: true,
         disableTap: true,
